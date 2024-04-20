70ec1c6b3cd4367a19333057f3ee481fbdb2fe4d
Isy <isy1997@freenet.de>
2024-04-20 21:24:25 +0200

    .

--- GameProject/obj/Debug/net8.0/GameProject.AssemblyInfo.cs
+++ GameProject/obj/Debug/net8.0/GameProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dfda95df4cd65902259617f797a3aebd088379")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
