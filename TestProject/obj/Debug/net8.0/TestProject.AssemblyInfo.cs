44873bedc5205ce67e277b83c7b08d7268da6c39
Isy <isy1997@freenet.de>
2024-04-24 20:37:55 +0200

    Practice writing readable code

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e902a830ea01ab505981eb87380df002f698cb7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5397e60b818bd2b5b1f61519bf5da1837d0b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
