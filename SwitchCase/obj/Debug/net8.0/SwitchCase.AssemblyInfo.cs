0a0f64d15e5a3030571f63645a5272b64ae0144a
Isy <isy1997@freenet.de>
2024-05-10 19:48:36 +0200

    Practice for loop

--- SwitchCase/obj/Debug/net8.0/SwitchCase.AssemblyInfo.cs
+++ SwitchCase/obj/Debug/net8.0/SwitchCase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f8c39aa26002e5aff87ccda69458f8a6c45f91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38778afd7d9d7dc57277f5a3b1386dc9eb323f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
