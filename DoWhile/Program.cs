3ac8e1406ac2f0c1903b4097d2781cd808e0ee41
Isy <isy1997@freenet.de>
2024-05-11 16:49:46 +0200

    Practice do while loop

---
+++ DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿Random random = new Random();
+int monster = 10;
+int hero = 10;
+
+do
+{
+    int attack = random.Next(1, 11);
+    monster -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    attack = random.Next(1, 11);
+    hero -= attack;
+    Console.WriteLine($"Hero was damaged and lost {attack} health and now has {hero} health.");
+} while  (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
